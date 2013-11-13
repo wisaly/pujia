e6ed0c7c5b93bb65c85ef1ea48ed5e5e16c638c6
Ma Qiming <wisaly@gmail.com>
2013-11-13 14:37:59 +0800

    心灵传说解压工具

---
+++ tohr_decompress/tohr_decompress/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace tohr_decompress
+{
+    class Program
+    {
+        static int totalFileCount = 0;
+        static int successFileCount = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("心灵传说解压缩工具");
+            Console.WriteLine("pujia.kris");
+
+            if (args.Length >= 1 && args[0] == "-d")
+            {
+                Console.WriteLine("将清除所有当前目录和子目录中的*.dec文件");
+                delDir(Directory.GetCurrentDirectory());
+                Console.WriteLine(
+                    string.Format("共处理文件{0}个",
+                    totalFileCount, successFileCount));
+                return;
+            }
+
+            Console.WriteLine("将解压所有当前目录和子目录中的*.dat;*.bin文件");
+            decompressDir(Directory.GetCurrentDirectory());
+            Console.WriteLine(
+                string.Format("共处理文件{0}个，成功{1}个，失败{2}",
+                totalFileCount, successFileCount, totalFileCount - successFileCount));
+        }
+
+        static void decompressDir(string dir)
+        {
+            DirectoryInfo theFolder = new DirectoryInfo(dir);
+
+            FileInfo[] fileInfo = theFolder.GetFiles();
+            foreach (FileInfo file in fileInfo)
+            {
+                if (file.Extension.ToLower() == ".dat"
+                    || file.Extension.ToLower() == ".bin")
+                {
+                    string decompressFile = file.FullName + ".dec";
+                    Console.Write("正在解压:{0}...", file.FullName);
+                    
+                    try
+                    {
+                        totalFileCount++;
+                        dat.decompress(file.FullName, decompressFile);
+                        successFileCount++;
+                        Console.WriteLine("成功.");
+                    }
+                    catch (System.Exception )
+                    {
+                        Console.WriteLine("失败.");
+                    }
+                }
+            }
+
+            DirectoryInfo[] subDirs = theFolder.GetDirectories();
+
+            foreach (DirectoryInfo subdir in subDirs)
+            {
+                decompressDir(subdir.FullName);
+            }
+        }
+
+        static void delDir(string dir)
+        {
+            DirectoryInfo theFolder = new DirectoryInfo(dir);
+
+            FileInfo[] fileInfo = theFolder.GetFiles();
+            foreach (FileInfo file in fileInfo)
+            {
+                if (file.Extension.ToLower() == ".dec")
+                {
+                    totalFileCount++;
+                    Console.WriteLine("正在删除文件:{0}", file.FullName);
+                    File.Delete(file.FullName);
+                }
+            }
+
+            DirectoryInfo[] subDirs = theFolder.GetDirectories();
+
+            foreach (DirectoryInfo subdir in subDirs)
+            {
+                delDir(subdir.FullName);
+            }
+        }
+    }
+}
---
+++ tohr_decompress/tohr_decompress/dat.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Firefly;
+using System.IO;
+
+namespace tohr_decompress
+{
+    class dat
+    {
+        static public void decompress(string infile,string outfile)
+        {
+            StreamEx sin = new StreamEx(infile, FileMode.Open, FileAccess.Read);
+            StreamEx sms = new StreamEx(new MemoryStream());
+
+            while (sin.Position < sin.Length)
+            {
+                byte p1 = sin.ReadByte();
+                byte p2 = 0;
+                byte p3 = 0;
+
+                if (p1 < 0x40)
+                {
+                    byte F = 0;
+                    if (p1 == 0)
+                    {
+                        //stepFlag = 6;
+
+                        p2 = sin.ReadByte();
+                        if (p2 >= 0x40)
+                        {
+                            F = (byte)(p2 - 0x40);
+                        }
+                        else
+                        {
+                            //stepFlag = 7;
+                            p3 = sin.ReadByte();
+                            p2 = (byte)(p2 * 2 + p3 * 4);
+                            if (p2 > 0)
+                            {
+                                p2 += 2;
+                            }
+                            F = p2;
+                        }
+                    }
+                    else
+                    {
+                        //stepFlag = 0;
+                        F = p1;
+                    }
+                    sms.WriteFromStream(sin,F);
+                }
+                else
+                {
+                    int S = 0; // 回退量
+                    int P = 0; // 复制量
+                    // 复制回退
+                    if (p1>0x7f)
+                    {
+                        //stepFlag = 4;
+                        p2 = sin.ReadByte();
+                        if ((p1 & 0x40) > 0)
+                        {
+                            //stepFlag = 5;
+                            p3 = sin.ReadByte();
+                            S = ((p2 & 0x7f) << 8) + p3 + 1;
+                            P = (p1 & 0x3f) * 2 + (p2 >> 7) + 4;
+                        }
+                        else
+                        {
+                            //stepFlag = 0;
+                            S = ((p1 & 0x3) << 8) + p2 + 1;
+                            P = ((p1 >> 2) & 0xf) + 3;
+                        }
+                    }
+                    else
+                    {
+                        //stepFlag = 0;
+                        S = (p1 & 0xf) + 1;
+                        P = (p1 / 0x10) - 2;
+                    }
+
+                    long curpos = sms.Position;
+                    while (P > 0)
+                    {
+                        sms.Position -= S;
+                        byte b = sms.ReadByte();
+                        sms.Position += S - 1;
+                        sms.WriteByte(b);
+                        P--;
+                    }
+                    
+                    
+                }
+            }
+
+            StreamEx sout = new StreamEx(outfile, FileMode.Create, FileAccess.Write);
+            sms.Position = 0;
+            sout.WriteFromStream(sms, sms.Length);
+            sout.Close();
+        }
+    }
+}
